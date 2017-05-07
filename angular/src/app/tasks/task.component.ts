84adda2c3193aca0246a2b9cc89e37333611377f
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2017-05-07 21:56:18 +0200

    Single task view and update

--- angular/src/app/tasks/task.component.ts
+++ angular/src/app/tasks/task.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'gtd-task',
   template: `
-    <p>
-      task Works!
-    </p>
+      <div class="row">
+      <div class="col-8">{{ task.description }}</div>
+      <div class="col-2">
+        <button class="btn btn-sm btn-block btn-success" style="width:100%; height:100%;" (click)="onComplete()">
+          <i class="fa fa-check" aria-hidden="true"></i>
+        </button>
+      </div>
+       <div class="col-2">
+        <button class="btn btn-sm btn-block btn-danger" style="width:100%; height:100%;" (click)="onDelete()">
+          <i class="fa fa-trash" aria-hidden="true"></i>
+        </button>
+      </div>
+      </div>
   `,
-  styles: []
+  styles: [`
+      .row {
+        border-top: 1px solid #eee;
+        border-bottom: 1px solid #eee;
+        margin: 2px auto;
+      }
+  `]
 })
 export class TaskComponent implements OnInit {
+
+  @Input() task: Task;
+  @Output() completeEmitter = new EventEmitter<number>();
+  @Output() deleteEmitter = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onComplete(): void {
+    this.completeEmitter.emit(this.task.id);
+  }
+
+  onDelete(): void {
+    this.deleteEmitter.emit(this.task.id);
+  }
 }
