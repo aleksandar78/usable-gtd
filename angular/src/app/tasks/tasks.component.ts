21b5a6f54c8e63b26dc5287f31fdd01f94e22612
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2017-05-07 19:06:56 +0200

    Generate tasks component

---
+++ angular/src/app/tasks/tasks.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gtd-tasks',
+  template: `
+    <p>
+      tasks Works!
+    </p>
+  `,
+  styles: []
+})
+export class TasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
