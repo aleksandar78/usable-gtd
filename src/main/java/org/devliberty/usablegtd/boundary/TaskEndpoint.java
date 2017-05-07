680305ee27db9dfb5a7fafbb3254185272c8a910
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2017-05-07 15:39:25 +0200

    Retrieve list of tasks

--- src/main/java/org/devliberty/usablegtd/boundary/TaskEndpoint.java
+++ src/main/java/org/devliberty/usablegtd/boundary/TaskEndpoint.java
@@ -1,15 +1,19 @@
 package org.devliberty.usablegtd.boundary;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -46,4 +50,11 @@ public class TaskEndpoint {
 		return Response.ok(opt.get()).build();
 	}
 
+	@GET
+	public List<Task> listAll(@QueryParam("start") final Integer startPosition,	@QueryParam("max") final Integer maxResult) {
+		// no pagination required for now
+		return repository.getAllForCurrentUser();
+	}
+
 }
+
