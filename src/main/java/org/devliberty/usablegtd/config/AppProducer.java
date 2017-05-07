b9fccd05453fc84ddace384e775e66db300ab0a8
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2017-05-07 14:37:42 +0200

    Expose EntityManager as Application Bean

--- src/main/java/org/devliberty/usablegtd/config/AppProducer.java
+++ src/main/java/org/devliberty/usablegtd/config/AppProducer.java
@@ -2,6 +2,8 @@ package org.devliberty.usablegtd.config;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -10,6 +12,10 @@ public class AppProducer {
 
 	public AppProducer() {}
 
+	@Produces
+	@PersistenceContext(unitName="usablegtd-persistence-unit")
+	EntityManager em;
+	
 	@Produces @GTD
 	public ObjectMapper mapper() {
 		return new ObjectMapper();
