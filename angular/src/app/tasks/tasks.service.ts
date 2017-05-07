5b001df149161c7e9a1fa80baffeddd543a2117c
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2017-05-07 20:32:34 +0200

    Call backend for pending tasks

--- angular/src/app/tasks/tasks.service.ts
+++ angular/src/app/tasks/tasks.service.ts
@@ -1,8 +1,20 @@
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import { Injectable } from '@angular/core';
+import { Task } from './task';
+import { Http } from '@angular/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class TasksService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  pendingTasks(): Observable<Task[]> {
+    return this.http.get(`${environment.endpointUrl}/tasks`)
+      .map(res => res.json())
+      .catch(err => Observable.throw(err.json().error || 'Task endpoint service unavailable'));
+  }
 
 }
