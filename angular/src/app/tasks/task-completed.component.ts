68439b0a6e21370f3dc4b16cba4c56292d517cb4
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2017-05-08 22:28:23 +0200

    Rename to task-completed

--- angular/src/app/tasks/task-completed.component.ts
+++ angular/src/app/tasks/task-completed.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'gtd-task-completed',
   template: `
-    <p>
-      task-completed Works!
-    </p>
+    <div class="row">
+      <div class="col-10 desc-text">{{ task.description }}</div>
+       <div class="col-2">
+        <button class="btn btn-sm btn-block btn-secondary" style="width:100%; height:100%;" (click)="taskDelete.emit(task.id)">
+          <i class="fa fa-trash" aria-hidden="true"></i>
+        </button>
+      </div>
+      </div>
   `,
-  styles: []
+  styles: [`
+    div.row:hover {
+      background-color: #eee;
+    }
+
+    .desc-text {
+      font-style: italic;
+    }
+  `]
 })
 export class TaskCompletedComponent implements OnInit {
+
+  @Input() task: Task;
+  @Output() taskDelete = new EventEmitter<number>();
 
   constructor() { }
 
