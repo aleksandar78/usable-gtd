96604a6d9ac36107abec0cc4ce034782e16c7ff3
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2017-05-08 20:55:07 +0200

    Generate component for completed tasks

---
+++ angular/src/app/tasks/task-completed.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gtd-task-completed',
+  template: `
+    <p>
+      task-completed Works!
+    </p>
+  `,
+  styles: []
+})
+export class TaskCompletedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
